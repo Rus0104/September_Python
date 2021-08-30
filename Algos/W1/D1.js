82932ccdf5d5904e638788709be80fb15a165220
Jin Kang <45414405+KangKyungJin@users.noreply.github.com>
2021-08-30 10:13:12 -0700

    first commit

---
+++ Algos/W1/D1.js
@@ -0,0 +1,56 @@
+/* case insensitive string compare */
+
+const strA1 = "ABC";
+const strB1 = "abc";
+const expected1 = true;
+
+const strA2 = "ABC";
+const strB2 = "abd";
+const expected2 = false;
+
+const strA3 = "ABC";
+const strB3 = "bc";
+const expected3 = false;
+
+/**
+ * Determines whether or not the strings are equal, ignoring case.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} strA
+ * @param {string} strB
+ * @returns {boolean} If the strings are equal or not.
+ */
+function caseInsensitiveStringCompare(strA, strB) {
+  return strA.toUpperCase() === strB.toUpperCase();
+}
+
+
+/* 
+  Acronyms
+  Create a function that, given a string, returns the string’s acronym 
+  (first letter of each word capitalized). 
+  Do it with .split first if you need to, then try to do it without
+*/
+
+const str1 = " there's no free lunch - gotta pay yer way. ";
+const expected1 = "TNFL-GPYW";
+
+const str2 = "Live from New York, it's Saturday Night!";
+const expected2 = "LFNYISN";
+
+/**
+ * Turns the given str into an acronym.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str A string to be turned into an acronym.
+ * @returns {string} The given str converted into an acronym.
+ */
+function acronymizeWithSplit(wordsStr) {
+  let acronym = "";
+  const wordsArr = wordsStr.split(" ");
+
+  for (const word of wordsArr) {
+    acronym += word[0].toUpperCase();
+  }
+  return acronym;
+}
