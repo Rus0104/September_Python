aedab1d69baacced7e5bc8491a98fd9173d64d8f
Jin Kang <45414405+KangKyungJin@users.noreply.github.com>
2021-09-08 12:09:13 -0700

    d7 push

---
+++ Algos/W2/D7.js
@@ -0,0 +1,58 @@
+/* 
+  Given a string,
+  return a new string with the duplicates excluded
+  Bonus: Keep only the last instance of each character.
+*/
+
+const str1 = "abcABC";
+const expected1 = "abcABC";
+
+const str2 = "helloo";
+const expected2 = "helo";
+
+/**
+ * De-dupes the given string.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str A string that may contain duplicates.
+ * @returns {string} The given string with any duplicate characters removed.
+ */
+function stringDedupe(str) {}
+
+/* 
+  Given a string containing space separated words
+  Reverse each word in the string.
+  If you need to, use .split to start, then try to do it without.
+*/
+
+const str1 = "hello";
+const expected1 = "olleh";
+
+const str2 = "hello world";
+const expected2 = "olleh dlrow";
+
+const str3 = "abc def ghi";
+const expected3 = "cba fed ihg";
+
+/**
+ * Reverses the letters in each words in the given space separated
+ * string of words. Does NOT reverse the order of the words themselves.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str Contains space separated words.
+ * @returns {string} The given string with each word's letters reversed.
+ */
+function reverseWords(str) {
+    var new_str = "";
+    var words = str.split(" ") // -> split will 'split' the string into an array/list at whereever you indicated it to split at. In this case we are splitting at " " (the space between the words). For example, if str is "hello world", str.split(" ") will return ['hello', 'world']
+
+    for(var word of words){
+        for(var i = word.length - 1; i >= 0; i--){
+            new_str += word[i]
+        }
+        new_str += " "
+    }
+    return new_str
+}
+
+console.log(reverseWords(str3))
